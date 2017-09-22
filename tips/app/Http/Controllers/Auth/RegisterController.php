c380fc235ffcaaf3daeef7f4cb90b18cffd25e5d
Ahmed <ghoul.ahmed5@gmail.com>
2017-09-22 17:21:11 +0100

    customize register page

--- tips/app/Http/Controllers/Auth/RegisterController.php
+++ tips/app/Http/Controllers/Auth/RegisterController.php
@@ -47,10 +47,14 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+
+//        dump($data);
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'username' => 'required|string|min:6||unique:users',
+            'dob' => 'required|date|before:2000-01-01',
         ]);
     }
 
@@ -65,6 +69,8 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'username' => $data['username'],
+            'dob' => $data['dob'],
             'password' => bcrypt($data['password']),
         ]);
     }
--- tips/database/migrations/2014_10_12_000000_create_users_table.php
+++ tips/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name',32);
+            $table->string('username',32)->unique();
+            $table->string('dob');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
